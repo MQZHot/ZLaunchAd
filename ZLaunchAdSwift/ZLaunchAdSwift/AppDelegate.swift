8b53e996d4aedfd570df99cdf9fd4d46e73b9754
mqz <mqz1228@163.com>
2017-12-29 23:47:25 +0800

    Objc添加启动图

--- ZLaunchAdSwift/ZLaunchAdSwift/AppDelegate.swift
+++ ZLaunchAdSwift/ZLaunchAdSwift/AppDelegate.swift
@@ -42,6 +42,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
 }
 extension AppDelegate {
     /// 模拟请求数据，此处解析json文件
