1d89bde744b992607683d3867f53903222492f8a
mqz <mqz1228@163.com>
2018-01-13 23:22:47 +0800

    添加Objc demo

--- ZLaunchAdObjc/ZLaunchAdObjc/ViewController.m
+++ ZLaunchAdObjc/ZLaunchAdObjc/ViewController.m
@@ -2,8 +2,8 @@
 //  ViewController.m
 //  ZLaunchAdObjc
 //
-//  Created by mengqingzheng on 2018/1/12.
-//  Copyright © 2018年 北京聚集科技有限公司. All rights reserved.
+//  Created by MQZHot on 2018/1/12.
+//  Copyright © 2018年 MQZHot. All rights reserved.
 //
 
 #import "ViewController.h"
