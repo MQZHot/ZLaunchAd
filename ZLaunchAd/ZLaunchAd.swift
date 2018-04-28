dd581c538a1113885085087b817233758446fd7b
mqz <mqz1228@163.com>
2018-04-28 17:30:46 +0800

    nothing

--- ZLaunchAd/ZLaunchAd.swift
+++ ZLaunchAd/ZLaunchAd.swift
@@ -5,7 +5,7 @@
 //  Created by MQZHot on 2017/12/30.
 //  Copyright © 2017年 MQZHot. All rights reserved.
 //
-//  https://github.com/MQZHot/ZLaunchAdVC
+//  https://github.com/MQZHot/ZLaunchAd
 //
 
 import UIKit
