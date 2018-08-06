be9e6f6260ff3a1a319018a0046955d30554ef00
MQZHot <mqz1228@163.com>
2018-08-06 16:19:58 +0800

    移除SwiftHash，使用CCommonCrypto进行MD5加密

---
+++ ZLaunchAdFramework/ZLaunchAdFramework/ZLaunchAdFramework.h
@@ -0,0 +1,19 @@
+//
+//  ZLaunchAdFramework.h
+//  ZLaunchAdFramework
+//
+//  Created by mengqingzheng on 2018/8/6.
+//  Copyright © 2018年 MQZHot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZLaunchAdFramework.
+FOUNDATION_EXPORT double ZLaunchAdFrameworkVersionNumber;
+
+//! Project version string for ZLaunchAdFramework.
+FOUNDATION_EXPORT const unsigned char ZLaunchAdFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZLaunchAdFramework/PublicHeader.h>
+
+
